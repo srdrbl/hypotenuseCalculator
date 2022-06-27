88e9b968493198a0edb1f845ed3baaa61d7f6092
Serdar <serdarbaal@gmail.com>
2022-06-27 20:45:50 +0300

    right triangle hypotenuse calculator

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.awt.desktop.SystemEventListener;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        double c;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the first edge: ");
+        a = input.nextInt();
+
+
+        System.out.print("Please enter the second edge: ");
+        b = input.nextInt();
+
+        c = Math.sqrt((a*a) + (b*b));
+        System.out.println("Hypotenuse: " + c);
+
+    }
+}
